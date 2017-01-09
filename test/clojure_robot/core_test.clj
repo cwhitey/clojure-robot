e716aa69051906589b6592d109122713e0730b8e
Callum White <callum.white@rea-group.com>
2017-01-10 10:42:57 +1100

    Initial commit. New blank template.

---
+++ test/clojure_robot/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-robot.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-robot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
