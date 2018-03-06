6f5f3d133f3a0bae0c9e89b32d1f2840443a11a1
Callum White <callumw1991@gmail.com>
2018-03-07 09:19:34 +1100

    Add new test

--- test/clojure_robot/core_test.clj
+++ test/clojure_robot/core_test.clj
@@ -7,4 +7,6 @@
   (testing "command parsing"
     (testing "place command"
       (is (= {:x 0 :y 0 :d :north}
-             (run-command "PLACE 0 0 NORTH"))))))
+             (run-command "PLACE 0 0 NORTH")))
+      (is (= {:x 0 :y 4 :d :east}
+             (run-command "PLACE 0 4 EAST"))))))
