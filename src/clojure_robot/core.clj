26d4978054714c658940304a7447e63bd06ab658
Callum White <callumw1991@gmail.com>
2019-08-26 23:35:49 +1000

    Refactor -main

--- src/clojure_robot/core.clj
+++ src/clojure_robot/core.clj
@@ -1,29 +1,28 @@
 (ns clojure-robot.core
-  (:require [clojure-robot.robot :as r]
+  (:require [clojure.string :refer [split-lines]]
+            [clojure-robot.robot :as r]
             [clojure-robot.util :refer :all]))
 
-(def place-re #"PLACE (\d) (\d) (.*)")
+(def place-regex #"PLACE (\d) (\d) (.*)")
 
 (defn run-command [robot command-string]
   (condp re-find command-string
-    place-re :>> (fn [[_ x y d]]
-                   (r/place
-                    (parse-int x)
-                    (parse-int y)
-                    (keyword (.toLowerCase d))))
+    place-regex :>> (fn [[_ x y d]]
+                      (r/place
+                       (parse-int x)
+                       (parse-int y)
+                       (keyword (.toLowerCase d))))
 
     #"MOVE"   (r/move robot)
     #"LEFT"   (r/left robot)
     #"RIGHT"  (r/right robot)
     #"REPORT" (r/report-status robot)))
 
-(defn run-program [program-string]
-  (let [command-list (clojure.string/split-lines program-string)
-        commands (drop-while #(not (re-matches place-re %)) command-list)]
-    commands
-    #_(reduce run-command [] (clojure.string/split-lines commands))))
+(defn run-program [command-list]
+  (let [commands (drop-while (complement (partial re-matches place-regex)) command-list)]
+    (reduce run-command [] commands)))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [command-input]
+  (run-program (split-lines command-input)))
